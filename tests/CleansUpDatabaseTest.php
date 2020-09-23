e2d293c8bfba5f36183bd2c6b5854c0fc0d34447
freek <freek@spatie.be>
2020-09-23 18:42:22 +0200

    wip

--- tests/CleansUpDatabaseTest.php
+++ tests/CleansUpDatabaseTest.php
@@ -9,24 +9,42 @@ use Spatie\LaravelQueuedDbCleanup\Tests\TestClasses\TestModel;
 
 class CleansUpDatabaseTest extends TestCase
 {
-    /** @test */
-    public function it_can_delete_records()
+    /**
+     * @test
+     *
+     * @dataProvider getTestCases
+     */
+    public function it_can_delete_records_in_the_right_amount_of_passes(
+        int $totalRecords,
+        int $chunkSize,
+        int $remaining,
+        int $passesPerformed)
     {
         Event::fake();
 
-        TestModel::factory()->count(1000)->create();
+        TestModel::factory()->count($totalRecords)->create();
 
         CleanDatabaseJobFactory::new()
             ->usingQuery(TestModel::query())
-            ->deleteChunkSize(100)
+            ->deleteChunkSize($chunkSize)
             ->dispatch();
 
-        $this->assertEquals(0, TestModel::count());
+        $this->assertEquals($remaining, TestModel::count());
 
-        Event::assertDispatched(function (CleanDatabaseCompleted $event) {
-            $this->assertEquals(11, $event->cleanConfig->pass);
+        Event::assertDispatched(function (CleanDatabaseCompleted $event) use ($passesPerformed) {
+            $this->assertEquals($passesPerformed, $event->cleanConfig->pass);
 
             return true;
         });
+    }
+
+    public function getTestCases(): array
+    {
+        return [
+            [100, 10, 0, 11],
+            [100, 10, 0, 11],
+            [99, 10, 0, 10],
+            [100, 5, 0, 21],
+        ];
     }
 }
